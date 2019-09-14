6cc82750260d4b6b9f4b315156712fbbeac82ccb
huizhuz <huizhuz@outlook.com>
2019-09-14 15:59:36 -0400

    Change the display of Welcome page

--- src/resume/Welcome.vue
+++ src/resume/Welcome.vue
@@ -1,25 +1,29 @@
 <template>
-	<div>
-		<div class="cover">
-			<div class="intro">
-				<h1>Huizhu Zhang</h1>
-				<h2>Welcome to my page</h2>
-				<h3>Front-end Developer | Language Educator</h3>
-				<p>A Software Engineer focused on Frontend development, experienced with building granular single page application. Actively searching fulltime positions or internships in front end development.</p>
-                <v-block></v-block>
-				<p><i>Skill set: <i></p>
-                <ul>
-                    <li>Proficient front-end developing skills with standardized HTML5, CSS3, JavaScript.
-Familiar with ES6 standard.</li>
-                    <li>Single page application development experience with ReactJS (JSX) and Vue.js.</li>
-                    <li>Great sensibility to web application design and responsive web development.
-Comfortable with Sass and third-party packages like Element and Bootstrap.</li>
-                    <li>Experience with Restful API, and study experience with R and Python.</li>
-                    <li>Comfortable with version control system (Git).</li>
-                    </ul>
-                <v-block></v-block>
-                <v-block></v-block>
-			</div>
+	<div class="cover">
+		<div class="intro">
+			<h1>Huizhu Zhang</h1>
+			<h2>Welcome to my page</h2>
+			<h3>Front-end Developer | Language Educator</h3>
+			<p>A Software Engineer focused on Frontend development, experienced with building granular single page application. Actively searching fulltime positions or internships in front end development.</p>
+			<v-block></v-block>
+			<p>
+				<i>Skill set:</i>
+			</p>
+			<ul>
+				<li>
+					Proficient front-end developing skills with standardized HTML5, CSS3, JavaScript.
+					Familiar with ES6 standard.
+				</li>
+				<li>Single page application development experience with ReactJS (JSX) and Vue.js.</li>
+				<li>
+					Great sensibility to web application design and responsive web development.
+					Comfortable with Sass and third-party packages like Element and Bootstrap.
+				</li>
+				<li>Experience with Restful API, and study experience with R and Python.</li>
+				<li>Comfortable with version control system (Git).</li>
+			</ul>
+			<v-block></v-block>
+			<v-block></v-block>
 		</div>
 	</div>
 </template>
@@ -31,11 +35,10 @@ Comfortable with Sass and third-party packages like Element and Bootstrap.</li>
 				rgba(20, 45, 116, 0.637),
 				rgba(20, 45, 116, 0.2)
 			),
-			url("./../images/welcome.jpeg")
-				no-repeat fixed;
+			url("./../images/welcome.jpeg") no-repeat fixed;
 		background-size: cover;
-    }	
-    
+	}
+
 	.intro {
 		color: white;
 		display: grid;
@@ -43,22 +46,22 @@ Comfortable with Sass and third-party packages like Element and Bootstrap.</li>
 		font-family: "Amatic SC", cursive;
 		padding: 1.5rem;
 		text-align: center;
-    }
-    h1 {
+	}
+	h1 {
 		font-size: 4rem;
 	}
 	h2 {
 		font-size: 2.5rem;
 	}
-    .intro p{
-        padding-top: 2rem;
-        font-family: 'Quicksand', sans-serif;
-        text-align: justify;
-        width: 70%;
-        margin: 0 auto;
-    }
-    ul{
-        list-style-type:none;
-        padding:0;
-    }
+	.intro p {
+		padding-top: 2rem;
+		font-family: "Quicksand", sans-serif;
+		text-align: justify;
+		width: 70%;
+		margin: 0 auto;
+	}
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
 </style>
