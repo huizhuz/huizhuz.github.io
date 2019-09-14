1ab8bba7fa4d2d3eb42e5071f08d32d08c448a3f
huizhuz <huizhuz@outlook.com>
2019-09-14 16:08:39 -0400

    Change project display

--- src/resume/projects/EachProject.vue
+++ src/resume/projects/EachProject.vue
@@ -1,16 +1,18 @@
 <template>
-	<div>
-		<h3>{{project.name}}</h3>
+	<div class="each-project">
+		<div>
+			<h3>{{project.name}}</h3>
 		<ul style="list-style-type:none;padding: 0;">
 			<li
 				v-for="item in project.description"
 				style="margin-bottom: 10px; text-align: justify; word-break: break-word;"
 			>{{item}}</li>
 		</ul>
-		<line-break></line-break>
-		<v-button 
-            :buttonName="project.buttonName"
-            @click.native="goToLink"></v-button>
+		</div>
+		<div>
+			<line-break></line-break>
+			<v-button :buttonName="project.buttonName" @click.native="goToLink"></v-button>
+		</div>
 	</div>
 </template>
 
@@ -18,16 +20,21 @@
 
 <script>
 	export default {
-        props: ["project"],
-        methods: {
-            goToLink(){
+		props: ["project"],
+		methods: {
+			goToLink() {
 				window.open(this.project.link);
-            }
-        }
+			}
+		}
 	};
 </script>
 
 <style scoped>
+	.each-project {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
 	h3 {
 		margin-top: 2.5rem;
 	}
