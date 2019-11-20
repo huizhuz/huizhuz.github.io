ce2e48d0da7d1c82637da69bc4a8e38cd90383fe
huizhuz <huizhuz@outlook.com>
2019-11-20 17:25:37 -0500

    Add confetti effect on project tab

--- src/Header.vue
+++ src/Header.vue
@@ -10,7 +10,7 @@
 			<div class="nav-wrapper">
 				<router-link to="/education" class="nav-item">Education</router-link>
 				<router-link to="/work" class="nav-item">Work</router-link>
-				<router-link to="/projects" class="nav-item">Projects</router-link>
+				<router-link to="/projects" class="nav-item confetti-button">Projects</router-link>
 				<router-link to="/teaching" class="nav-item">Teaching</router-link>
 				<router-link to="/contact" class="nav-item">Contact Me</router-link>
 			</div>
@@ -241,6 +241,102 @@
 		}
 		.slide-leave-to {
 			transform: translate(-100%, 0);
+		}
+	}
+
+	body {
+		font-size: 16px;
+		font-family: "Roboto", "Arial", sans-serif;
+		text-align: center;
+		background-color: #f8faff;
+	}
+	h1 {
+		margin-top: 150px;
+	}
+	.confetti-button {
+		position: relative;
+	}
+
+	.confetti-button:before,
+	.confetti-button:after {
+		position: absolute;
+		content: "";
+		display: block;
+		width: 140%;
+		height: 100%;
+		left: -10%;
+		z-index: -1000;
+		transition: all ease-in-out 0.5s;
+		background-repeat: no-repeat;
+	}
+
+	.confetti-button:before {
+		display: none;
+		top: -75%;
+		background-image: radial-gradient(circle, #ff8800 20%, transparent 20%),
+			radial-gradient(circle, transparent 20%, #ff0081 20%, transparent 30%),
+			radial-gradient(circle, #8ef03e 20%, transparent 20%),
+			radial-gradient(circle, #3f2bf6 20%, transparent 20%),
+			radial-gradient(circle, transparent 10%, #ff0081 15%, transparent 20%),
+			radial-gradient(circle, #0051ff 20%, transparent 20%),
+			radial-gradient(circle, #f7401f 20%, transparent 20%),
+			radial-gradient(circle, #ffa600 20%, transparent 20%),
+			radial-gradient(circle, #ffe70b 20%, transparent 20%);
+		background-size: 10% 10%, 20% 20%, 15% 15%, 20% 20%, 18% 18%, 10% 10%,
+			15% 15%, 10% 10%, 18% 18%;
+	}
+
+	.confetti-button:after {
+		display: none;
+		bottom: -75%;
+		background-image: radial-gradient(circle, #ff0081 20%, transparent 20%),
+			radial-gradient(circle, #ff9900 20%, transparent 20%),
+			radial-gradient(circle, transparent 10%, #ff1e00 15%, transparent 20%),
+			radial-gradient(circle, #ffee00 20%, transparent 20%),
+			radial-gradient(circle, #d0ff00 20%, transparent 20%),
+			radial-gradient(circle, #0044ff 20%, transparent 20%),
+			radial-gradient(circle, #ffffff 20%, transparent 20%);
+		background-size: 15% 15%, 20% 20%, 18% 18%, 20% 20%, 15% 15%, 10% 10%,
+			20% 20%;
+	}
+
+	.confetti-button:before {
+		display: block;
+		animation: topBubbles ease-in-out 0.75s forwards infinite;
+	}
+
+	.confetti-button:after {
+		display: block;
+		animation: bottomBubbles ease-in-out 0.75s forwards infinite;
+	}
+	@keyframes topBubbles {
+		0% {
+			background-position: 5% 90%, 10% 90%, 10% 90%, 15% 90%, 25% 90%, 25% 90%,
+				40% 90%, 55% 90%, 70% 90%;
+		}
+		50% {
+			background-position: 0% 80%, 0% 20%, 10% 40%, 20% 0%, 30% 30%, 22% 50%,
+				50% 50%, 65% 20%, 90% 30%;
+		}
+		100% {
+			background-position: 0% 70%, 0% 10%, 10% 30%, 20% -10%, 30% 20%, 22% 40%,
+				50% 40%, 65% 10%, 90% 20%;
+			background-size: 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%;
+		}
+	}
+	@keyframes bottomBubbles {
+		0% {
+			background-position: 10% -10%, 30% 10%, 55% -10%, 70% -10%, 85% -10%,
+				70% -10%, 70% 0%;
+		}
+		50% {
+			background-position: 0% 80%, 20% 80%, 45% 60%, 60% 100%, 75% 70%,
+				95% 60%, 105% 0%;
+		}
+		100% {
+			background-position: 0% 90%, 20% 90%, 45% 70%, 60% 110%, 75% 80%,
+				95% 70%, 110% 10%;
+			background-size: 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%;
 		}
 	}
 </style>
